418a4418468dd4de3e3a16b9448bc1d43c2f782c
Jimpei Yamamoto <zinpei104@gmail.com>
2022-07-03 21:47:50 +0900

    Add: sendAPI

--- main.go
+++ main.go
@@ -89,7 +89,68 @@ func withdraw(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func send(w http.ResponseWriter, r *http.Request) {
+	fromNum1Qs := r.URL.Query().Get("from")
+	fromNum2Qs := r.URL.Query().Get("to")
+	amountqs := r.URL.Query().Get("amount")
+	if fromNum1Qs == "" || fromNum2Qs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+	fromNum1, err := strconv.ParseFloat(fromNum1Qs, 64)
+	if err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+		return
+	}
+	fromNum2, err := strconv.ParseFloat(fromNum2Qs, 64)
+	if err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+		return
+	}
+	amount, err := strconv.ParseFloat(amountqs, 64)
+	if err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+		return
+	}
+	if amount <= 0 {
+		fmt.Fprint(w, "the amount to deposit should be greater than zero\n")
+		return
+	}
+	from, ok := accounts[fromNum1]
+	if !ok {
+		fmt.Fprintf(w, "Account with number %v can't be found!", fromNum1)
+		return
+	}
+	to, ok := accounts[fromNum2]
+	if !ok {
+		fmt.Fprintf(w, "Account with number %v can't be found!", fromNum2)
+		return
+	}
+	err = from.Withdraw(amount)
+	if err != nil {
+		fmt.Fprintf(w, "残金が足りません")
+		return
+	}
+	err = to.Deposit(amount)
+	if err != nil {
+		fmt.Fprintf(w, "Depositに失敗")
+		return
+	}
+	fmt.Fprint(w, from.Statement())
+	fmt.Fprint(w, "\n")
+	fmt.Fprint(w, to.Statement())
+}
+
 func main() {
+	accounts[1] = &bankcore.Account{
+		Customer: bankcore.Customer{
+			Name:    "yjimpei",
+			Address: "Japan",
+			Phone:   "777 7777 7777",
+		},
+		Number:  1,
+		Balance: 1000,
+	}
 	accounts[1001] = &bankcore.Account{
 		Customer: bankcore.Customer{
 			Name:    "John",
@@ -102,5 +163,6 @@ func main() {
 	http.HandleFunc("/statement", statement)
 	http.HandleFunc("/deposit", deposit)
 	http.HandleFunc("/withdraw", withdraw)
+	http.HandleFunc("/send", send)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
